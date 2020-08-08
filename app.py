caf134e8364196e680a9b7c394c2abfcd784b043
Aaditya1978 <aadityasinghal1978@gmail.com>
2020-08-08 15:33:23 +0530

    added basic visualizaton of dataset

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
 import streamlit as st
 import pandas as pd 
 import numpy as np 
+from feature_eng import *
 
-st.subheader("Exploratory Data Analysis")
+
+activities = ["Home", "EDA", "Model Building", "About UsS"]	
+choice = st.sidebar.selectbox("Select Option",activities)
 st.set_option('deprecation.showfileUploaderEncoding', False)
-data = st.file_uploader("Upload a Dataset", type=["csv"])
-if data != None :
-    df = pd.read_csv(data)
-    st.dataframe(df.head())
+
+if choice == "Home":
+    st.markdown("<h1 style='text-align: center; color: green;'>Exploratory data analysis</h1>", unsafe_allow_html=True)
+    st.text("")
+    st.text("")
+    data = st.file_uploader("Upload The Dataset", type=["csv"])
+    if data != None :
+        df = pd.read_csv(data)
+        st.text("")
+        st.text("")        
+        st.subheader("Head the Dataset : ")
+        st.text("")
+        st.dataframe(df.head())
+        st.text("")
+        st.subheader("Shape of the Dataset : ")
+        st.text("")
+        st.write(df.shape)
+        st.text("")
+        st.text("")
+        dic = type_of_feature(df)
+        st.subheader("Categories of different Features are : ")
+        st.write(dic)
+        st.text("")
+        missing_values_count = null_value(df)
+        st.subheader("The Missing Values In Dataset and Strategey to Fill them : ")
+        st.write(missing_values_count)
