b8a59857b88c861bb7f66b393b062a16d11d85b7
Aaditya1978 <aadityasinghal1978@gmail.com>
2020-08-08 18:25:17 +0530

    added graphs for viewing feature enginnering

--- app.py
+++ app.py
@@ -33,3 +33,33 @@ if choice == "Home":
         missing_values_count = null_value(df)
         st.subheader("The Missing Values In Dataset and Strategey to Fill them : ")
         st.write(missing_values_count)
+        st.text("")
+        st.text("")
+        st.write("Null Values in Heatmap Form")
+        st.text("")
+        heat_plot = heatmap_generator(df.isnull())
+        st.pyplot()
+        st.text("")
+        st.text("")
+        st.subheader("Imbalanced Features in Dataset are : ")
+        ls = imbalanced_feature(df)
+        if ls == []:
+            st.write("There are no imbalanced Features in Dataset")
+        else:
+            st.dataframe(ls)
+        st.text("")
+        st.text("")
+        categorical = cat_num(df)
+        categorical_feature = st.selectbox("Select Categorical Feature", categorical)
+        percent_pie = prcntage_values( categorical_feature, df)
+        st.write(percent_pie)
+        st.pyplot()
+        st.subheader("Two features categorical values combined comparator")
+        categorical1 = st.selectbox("Select First Categorical Feature", categorical)
+        categorical2 = st.selectbox("Select Second Categorical Feature", categorical)
+        cat_lis = [categorical1, categorical2]
+        comp_plot = different_cat_comparator(cat_lis, df)
+        st.write(comp_plot)
+        st.pyplot()
+
+       
