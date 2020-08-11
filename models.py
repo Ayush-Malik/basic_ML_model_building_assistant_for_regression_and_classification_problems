7d5027f1e8a5e32daaf42885a1a9c21a7d81c985
Abhay_Dhiman <abhaysdhimans@gmail.com>
2020-08-11 11:32:24 +0530

    Improve name and doc for the method(now--> model_call) of Models class

--- models.py
+++ models.py
@@ -7,11 +7,11 @@ from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
 from xgboost import XGBClassifier, XGBRegressor
 
 
-def linear_regression(X_train, y_train):
+def linear_regression(X_train, X_test, y_train, y_test):
     regressor = LinearRegression()
     regressor.fit(X_train, y_train)
-    y_pred = regressor.predict(X_test)      #X_test ---- remaining 
-    print('R2 score--> ', regressor.score(X_test, y_test))          #y_test ---- remaining
+    y_pred = regressor.predict(X_test)
+    print('R2 score--> ', regressor.score(X_test, y_test))
     return y_pred
 
 
@@ -34,27 +34,34 @@ class Models:
         self.y_test = y_list[1]
         self.model_list = model_list
         self.dict = dict()
-    
-    def model_num(self):
+
+    def model_call(self):
+        '''
+        model_call makes different function calls according to the Attributes received through model_list.
+        '''
         if self.model_list == None:
-            
+            # To track for model_list is empty or not
             return "No model selected; model_list got None Attribute"
+        
         elif type(self.model_list) != list:
-            
+            # To track type for model_list
             return f"Could not recognize {type(self.model_list)} object; List object must be passed"
+        
         else:
-            
+            # For calling respective functions according to the model_list
             model_list_len = len(self.model_list)
             print("Working on it! please wait for a while")
+            
             for model_list_index in range(model_list_len):
-                
                 if self.model_list[model_list_index] == 'LinearRegression':
-                    
-                    pred_output = linear_regression(self.X_train, self.y_train)
+                    pred_output = linear_regression(
+                        self.X_train, self.X_test,
+                        self.y_train, self.y_test
+                        )
                     self.dict['LinearRegression'] = pred_output
 
 
-print(Models("x", "y", ["LinearRegression"]).model_num())
+print(Models("x", "y", ["LinearRegression"]).model_call())
 
 
 # function for finding different scores ---- remaining
