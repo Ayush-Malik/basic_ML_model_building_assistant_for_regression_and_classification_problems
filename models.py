0907f0ea0be7e825a4dc0f5c0051a287be5e003c
Abhay_Dhiman <abhaysdhimans@gmail.com>
2020-08-11 11:13:54 +0530

    Updated parameters and doc for init method

--- models.py
+++ models.py
@@ -16,9 +16,22 @@ def linear_regression(X_train, y_train):
 
 
 class Models:
-    def __init__(self, X_train, y_train, model_list = None):
-        self.X_train = X_train
-        self.y_train = y_train
+    def __init__(self, X_list, y_list, model_list = None):
+        '''
+        Models is used to train different models on the given parameters.
+        X_list:- list for X_train and X_test, order is important.
+        y_list:- list for y_train and y_test, order is important.
+        model_list:- takes in different model names, must pass in a list object.
+        
+        Examples
+        ========
+        >>> Models([X_train, X_test], [y_train, y_test], ["LinearRegression", "RandomForestRegressor"])
+        >>> Models([X_train, X_test], [y_train, y_test], ["DecisionTreeClassifier", "RandomForestClassifier"])
+        '''
+        self.X_train = X_list[0]
+        self.X_test = X_list[1]
+        self.y_train = y_list[0]
+        self.y_test = y_list[1]
         self.model_list = model_list
         self.dict = dict()
     
