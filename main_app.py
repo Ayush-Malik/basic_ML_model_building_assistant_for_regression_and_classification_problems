ce513ad8991212214dee077e32d63bf0125ee984
Ayush Malik <ayushmalik779@gmail.com>
2020-08-19 11:36:32 +0530

    Modularized entire app

--- main_app.py
+++ main_app.py
@@ -6,34 +6,39 @@ import os
 
 link2 = "<link href='https://fonts.googleapis.com/css2?family=Lato:ital,wght@1,700&display=swap' rel='stylesheet'>"
 markdown_style_sidebar = "text-align: center; font-family: Georgia, Times, serif; font-weight: bolder; font-size:40px; padding-top: 20px; background-image: linear-gradient(to left, rgb(0, 179, 60), rgb(0, 179, 134), blue); - webkit-background-clip: text; - moz-background-clip: text; background-clip: text; color: transparent; "
-markdown_style_h1      = "font-size:40px; color:black; font-family: lato, sans-serif;"
-markdown_style_h2      = "font-family: lato, sans-serif; font-size: 20px; font-variant: normal; font-weight: 700; line-height: 15.4px; position:relative; left:30px; color:grey"
+markdown_style_h1 = "font-size:40px; color:black; font-family: lato, sans-serif;"
+markdown_style_h2 = "font-family: lato, sans-serif; font-size: 20px; font-variant: normal; font-weight: 700; line-height: 15.4px; position:relative; left:30px; color:grey"
 
 
 set_option('deprecation.showfileUploaderEncoding', False)
-activities = ["Home", "EDA", "Model Building", "About Us"]	
+activities = ["Home", "EDA", "Model Building", "About Us"]
 
 
-sidebar.markdown("<p style='" + markdown_style_sidebar +"' >" + "ML Automator" + "</p>", unsafe_allow_html=True)
+sidebar.markdown("<p style='" + markdown_style_sidebar +
+                 "' >" + "ML Automator" + "</p>", unsafe_allow_html=True)
 sidebar.text("")
 sidebar.text("")
 
-choice = sidebar.selectbox("Select Option",activities)
+choice = sidebar.selectbox("Select Option", activities)
+
 
 def image_maker(image_name):
     with open(image_name, "rb") as img_file:
         my_string = base64.b64encode(img_file.read()).decode()
-        imigi = "<img style='position:fixed; right:1px;bottom:1px; width:60px; height:60px; border-radius: 50%;' src='data:image/png;base64,{}' class='img-fluid'>".format(my_string)
+        imigi = "<img style='position:fixed; right:1px;bottom:1px; width:60px; height:60px; border-radius: 50%;' src='data:image/png;base64,{}' class='img-fluid'>".format(
+            my_string)
         return imigi
 
-image1 = image_maker("git_icon.png")
 
-sidebar.text("") 
+image1 = image_maker("images/git_icon.png")
+
 sidebar.text("")
 sidebar.text("")
+sidebar.text("")
 
-sidebar.markdown(link2 + "<p style='" + markdown_style_h1 +"' >" + "Developed by : " + "</p>", unsafe_allow_html=True)
-sidebar.text("") 
+sidebar.markdown(link2 + "<p style='" + markdown_style_h1 +
+                 "' >" + "Developed by : " + "</p>", unsafe_allow_html=True)
+sidebar.text("")
 
 sidebar.markdown("<a href='https://www.linkedin.com/in/ayush-malik-2252b7199/' target='_blank' style='display:inline-block;font-size:1.3em;width:2.5em;height:2.5em;line-height:2.5em;text-align:center;border-radius:50%;background:#000066;vertical-align:middle;margin-right:1em;color:white;position:static;left:10px;'>AM</a>", unsafe_allow_html=True)
 
@@ -41,52 +46,57 @@ sidebar.markdown("<a href='https://www.linkedin.com/in/abhay-dhiman-409378191/' 
 
 sidebar.markdown("<a href='https://www.linkedin.com/in/aaditya-singhal-a46720192/' target='_blank' style='display:inline-block;font-size:1.3em;width:2.5em;height:2.5em;line-height:2.5em;text-align:center;border-radius:50%;background:#00b33c;vertical-align:middle;margin-right:1em;color:white;position:static;margin-left:120px;margin-top:-193px'>AS</a>", unsafe_allow_html=True)
 
-sidebar.markdown("<p style='position: static; left:200px; margin-top:-87px; font-size:20px; font-weight:800;'>@Pro_Coders</p>", unsafe_allow_html=True)
+sidebar.markdown(
+    "<p style='position: static; left:200px; margin-top:-87px; font-size:20px; font-weight:800;'>@Pro_Coders</p>", unsafe_allow_html=True)
 
 sidebar.markdown(
-    "<a href='https://github.com/Ayush-Malik/basic_ML_model_building_assistant_for_regression_and_classification_problems/tree/streamlit_autoML' target='_blank'>" + image1 +"</a>",
+    "<a href='https://github.com/Ayush-Malik/basic_ML_model_building_assistant_for_regression_and_classification_problems/tree/streamlit_autoML' target='_blank'>" + image1 + "</a>",
     unsafe_allow_html=True
-    )
+)
 
 
-if choice == "Home": # For Navigating to Home Page
+if choice == "Home":  # For Navigating to Home Page
     Home()
 
-elif choice == "EDA": # For Navigating to EDA Page only when there's no null values in dataframe
-    
-    Markdown_Style('Exploratory data analysis' , 3)
+elif choice == "EDA":  # For Navigating to EDA Page only when there's no null values in dataframe
+
+    Markdown_Style('Exploratory data analysis', 3)
     text("")
     text("")
 
     df = pd.read_csv('update.csv')
 
-    if sum( df.isnull().sum() ) != 0:
-        subheader("Oops something went wrong , looks like there are Null values in DataFrame")
+    if sum(df.isnull().sum()) != 0:
+        subheader(
+            "Oops something went wrong , looks like there are Null values in DataFrame")
         text("")
         text("")
-        null_df = df.isnull().sum().sort_values( ascending = False).to_frame().reset_index().rename(columns = {'index' : 'Feature' , 0 : 'Null val count'})
-        dataframe( null_df  , width = 1000 , height = 1000  )
+        null_df = df.isnull().sum().sort_values(ascending=False).to_frame(
+        ).reset_index().rename(columns={'index': 'Feature', 0: 'Null val count'})
+        dataframe(null_df, width=1000, height=1000)
 
     else:
         EDA()
 
-elif choice == 'Model Building': # For Navigating to Model Building page only when there's no null values in df
-    
-    Markdown_Style('Model Building And Training' , 3)
+elif choice == 'Model Building':  # For Navigating to Model Building page only when there's no null values in df
+
+    Markdown_Style('Model Building And Training', 3)
     text("")
     text("")
 
     df = pd.read_csv('update.csv')
 
-    if sum( df.isnull().sum() ) != 0:
-        subheader("Oops something went wrong , looks like there are Null values in DataFrame")
+    if sum(df.isnull().sum()) != 0:
+        subheader(
+            "Oops something went wrong , looks like there are Null values in DataFrame")
         text("")
         text("")
-        null_df = df.isnull().sum().sort_values( ascending = False).to_frame().reset_index().rename(columns = {'index' : 'Feature' , 0 : 'Null val count'})
-        dataframe( null_df  , width = 1000 , height = 1000  )
+        null_df = df.isnull().sum().sort_values(ascending=False).to_frame(
+        ).reset_index().rename(columns={'index': 'Feature', 0: 'Null val count'})
+        dataframe(null_df, width=1000, height=1000)
 
-    else:    
+    else:
         Model_Builder()
 
-elif choice == 'About Us': # For Navigating to About Us Page
+elif choice == 'About Us':  # For Navigating to About Us Page
     About_Us()
--- modules/Home_Page_Functions.py
+++ modules/Home_Page_Functions.py
@@ -1,13 +1,13 @@
 from streamlit import *
-import pandas as pd 
-import numpy as np 
-from feature_eng import *
+import pandas as pd
+import numpy as np
+from modules.data_preprocessing import *
 import base64
+
 #############################################################################################################################################################################################
 
 
-
-def Markdown_Style(value , type = 1 , size = None , border_box = True):
+def Markdown_Style(value, type=1, size=None, border_box=True):
     # Markdown Styles
     length = len(value)
     link = "<link href='https://fonts.googleapis.com/css?family=Anton' rel='stylesheet'>"
@@ -24,54 +24,53 @@ def Markdown_Style(value , type = 1 , size = None , border_box = True):
     else:
         border_code = ""
 
-
-    markdown_style2 =  "position:relative; font-family:Anton;"+ size_code + border_code  +"padding: 20px;width: "+str(length*20)+"px;height: 100px;text-align:center;"
+    markdown_style2 = "position:relative; font-family:Anton;" + size_code + border_code + \
+        "padding: 20px;width: "+str(length*20) + \
+        "px;height: 100px;text-align:center;"
     markdown_style3 = "text-align: center; font-family: anton; font-weight: 300; font-size:60px; padding-top: 20px; background-image: linear-gradient(to left, #7c0909, #09477c, green); - webkit-background-clip: text; - moz-background-clip: text; background-clip: text; color: transparent; "
-    
+
     if type == 1:
         style_type = markdown_style1
         markdown(link2 + "<p style='" + style_type +
-            "' >" + value + "</p>", unsafe_allow_html=True)
+                 "' >" + value + "</p>", unsafe_allow_html=True)
 
     elif type == 2:
         value = value.upper()
         style_type = markdown_style2
         markdown(link + "<p style='" + style_type +
-            "' >" + value + "</p>", unsafe_allow_html=True)
-    
+                 "' >" + value + "</p>", unsafe_allow_html=True)
+
     elif type == 3:
         style_type = markdown_style3
         value = value.upper()
         markdown(link + "<p style='" + style_type +
-            "' >" + value + "</p>", unsafe_allow_html=True)
-
-
+                 "' >" + value + "</p>", unsafe_allow_html=True)
 
 
 #############################################################################################################################################################################################
-
-
 ''' As the name suggests the given function contains important 
     functionalities of streamlit at a single place '''
-def Cool_Data_Printer( sub_header = None , markdown_type_1 = None , markdown_type_2 = None ,  data_frame = None , write_this = None , print_info = None):
+
+
+def Cool_Data_Printer(sub_header=None, markdown_type_1=None, markdown_type_2=None,  data_frame=None, write_this=None, print_info=None):
     text("")
     text("")
-        
+
     if sub_header is not None:
-        subheader( sub_header )
+        subheader(sub_header)
 
     if markdown_type_1 is not None:
-        Markdown_Style( markdown_type_1 , 1 )
+        Markdown_Style(markdown_type_1, 1)
 
     if markdown_type_2 is not None:
-        Markdown_Style( markdown_type_2 , 2 )
-    
+        Markdown_Style(markdown_type_2, 2)
+
     if data_frame is not None:
-        dataframe( data_frame.head() )
-    
+        dataframe(data_frame.head())
+
     if write_this is not None:
-        write( write_this )
-    
+        write(write_this)
+
     if print_info is not None:
         info(print_info)
 
@@ -81,24 +80,26 @@ def Cool_Data_Printer( sub_header = None , markdown_type_1 = None , markdown_typ
 
 ''' This particular function is used to print plots using 'pyplot' 
     or 'plotly_chart' functions of streamlit with a markdown_type_1 '''
-def Cool_Plot_Printer(plot , sub_header = None , markdown_type_1 = None , markdown_type_2 = None  , markdown_type_3 = None , plot_print_type = None  ):
+
+
+def Cool_Plot_Printer(plot, sub_header=None, markdown_type_1=None, markdown_type_2=None, markdown_type_3=None, plot_print_type=None):
 
     if sub_header is not None:
-        subheader( sub_header )
+        subheader(sub_header)
 
     if markdown_type_1 is not None:
-        Markdown_Style( markdown_type_1 , 1 )
+        Markdown_Style(markdown_type_1, 1)
 
     if markdown_type_2 is not None:
-        Markdown_Style( markdown_type_2 , 2 )
-    
+        Markdown_Style(markdown_type_2, 2)
+
     if markdown_type_3 is not None:
-        Markdown_Style( markdown_type_3 , 3 )
-    
+        Markdown_Style(markdown_type_3, 3)
+
     if plot_print_type == 'pyplot':
         pyplot()
     elif plot_print_type == 'plotly_chart':
-        plotly_chart( plot )
+        plotly_chart(plot)
 
 
 #############################################################################################################################################################################################
@@ -106,56 +107,62 @@ def Cool_Plot_Printer(plot , sub_header = None , markdown_type_1 = None , markdo
 
 ''' This particular function provides [ checkbox + selectbox / Multiselect ] 
     functionalitis and it return , it returns a list of features selected by user '''
-def Cool_Data_Plotter(df , checkbox_text , drop_down_list  , plot_type , sub_header = None , markdown_type_1 = None , markdown_type_2 = None , markdown_type_3 = None ,    select_box_text_type_1 = None  , select_box_text_type_2 = None , multi_select_box_text = None   ):
-    
+
+
+def Cool_Data_Plotter(df, checkbox_text, drop_down_list, plot_type, sub_header=None, markdown_type_1=None, markdown_type_2=None, markdown_type_3=None,    select_box_text_type_1=None, select_box_text_type_2=None, multi_select_box_text=None):
+
     if sub_header is not None:
-        subheader( sub_header )
+        subheader(sub_header)
 
     if markdown_type_1 is not None:
-        Markdown_Style( markdown_type_1 , 1 )
+        Markdown_Style(markdown_type_1, 1)
 
     if markdown_type_2 is not None:
-        Markdown_Style( markdown_type_2 , 2 )
-    
+        Markdown_Style(markdown_type_2, 2)
+
     if markdown_type_3 is not None:
-        Markdown_Style( markdown_type_3 , 3 )
-    
-    if checkbox( checkbox_text ):
+        Markdown_Style(markdown_type_3, 3)
 
-        if select_box_text_type_1 is not None: # Single Checkbox
+    if checkbox(checkbox_text):
 
-            categorical_feature = selectbox( select_box_text_type_1 , drop_down_list , key = 183737487)
+        if select_box_text_type_1 is not None:  # Single Checkbox
+
+            categorical_feature = selectbox(
+                select_box_text_type_1, drop_down_list, key=183737487)
 
             if categorical_feature != drop_down_list[0]:
                 unique_len = len(df[categorical_feature].value_counts())
                 if unique_len > 15:
                     dataframe(df[categorical_feature].value_counts())
-                    Markdown_Style("Total unique values : " + str(unique_len) , 1)
+                    Markdown_Style("Total unique values : " +
+                                   str(unique_len), 1)
                 elif plot_type == 'pie_chart':
                     # percent_pie = prcntage_values( categorical_feature, df)
-                    fig = suplots_maker_for_table_and_piechart(df , type_null = True , feature = categorical_feature )
+                    fig = suplots_maker_for_table_and_piechart(
+                        df, type_null=True, feature=categorical_feature)
                     plotly_chart(fig)
 
-        elif select_box_text_type_2 is not None: # Two Checkboxes
-            
+        elif select_box_text_type_2 is not None:  # Two Checkboxes
+
             drop_down_list[0] = "Choose the First Feature"
-            first_one           = drop_down_list
+            first_one = drop_down_list
 
-            categorical1 = selectbox( select_box_text_type_2[0] , first_one , key = 9929389 )
+            categorical1 = selectbox(
+                select_box_text_type_2[0], first_one, key=9929389)
 
-            if categorical1 != drop_down_list[0]: # 2nd selectbox will appear only when 1st is already selected---selectbox -> 1 
-                
+            # 2nd selectbox will appear only when 1st is already selected---selectbox -> 1
+            if categorical1 != drop_down_list[0]:
+
                 drop_down_list[0] = "Choose the Second Feature"
-                second_one           = drop_down_list
+                second_one = drop_down_list
                 second_one.remove(categorical1)
 
-                categorical2 = selectbox( select_box_text_type_2[1] , second_one ) # ---selectbox -> 2
+                categorical2 = selectbox(
+                    select_box_text_type_2[1], second_one)  # ---selectbox -> 2
 
                 if categorical2 != drop_down_list[0]:
-                    cat_lis          = [categorical1, categorical2] 
-                    comparison_plot  = two_cat_comparator( cat_lis , df )
-
-
+                    cat_lis = [categorical1, categorical2]
+                    comparison_plot = two_cat_comparator(cat_lis, df)
 
                     if plot_type == 'comparison_plot':
                         try:
@@ -163,9 +170,9 @@ def Cool_Data_Plotter(df , checkbox_text , drop_down_list  , plot_type , sub_hea
                         except:
                             dataframe(comparison_plot)
                             text("")
-                            info( "x-axis--> "+ categorical1 + ", y-axis--> " + categorical2 )
-                            
-                        
+                            info("x-axis--> " + categorical1 +
+                                 ", y-axis--> " + categorical2)
+
     text("")
     text("")
     text("")
@@ -177,11 +184,13 @@ def Cool_Data_Plotter(df , checkbox_text , drop_down_list  , plot_type , sub_hea
 ''' This function accepts a df and drop all 
     the features selected by user and returns a feature_tracker
     list which contains active null_val feature '''
+
+
 def feature_dropper(df):
     markdown_type_1 = "Select the feature to be dropped : "
-    Markdown_Style(markdown_type_1 , 2)
+    Markdown_Style(markdown_type_1, 2)
     missing_lis = missing_value_lis(df)
-    lis_drop = multiselect("", missing_lis) 
+    lis_drop = multiselect("", missing_lis)
     feature_tracker, sent = drop_feat(df, lis_drop)
     if lis_drop != []:
         success(sent)
@@ -190,30 +199,28 @@ def feature_dropper(df):
     return feature_tracker
 
 
-
 #############################################################################################################################################################################################
 
-        
-def missing_values_filling_system(df , feature_tracker):
-    Markdown_Style( "Select Features to be filled" , 2)
+
+def missing_values_filling_system(df, feature_tracker):
+    Markdown_Style("Select Features to be filled", 2)
     lis_fill = []
     feature_ch = []
     count = 0
     for feature in feature_tracker:
         if checkbox(feature):
             if df.dtypes[feature] == 'object':
-                stratigies_lis = ["strategy" ,  "mode"]
+                stratigies_lis = ["strategy",  "mode"]
             else:
-                stratigies_lis = ["strategy","mean", "median"]
-                 
+                stratigies_lis = ["strategy", "mean", "median"]
 
-            strategy = selectbox("Choose strategy", stratigies_lis , key = count)
+            strategy = selectbox("Choose strategy", stratigies_lis, key=count)
             if strategy != "strategy":
                 lis_fill.append(strategy)
                 feature_ch.append(feature)
                 success("Feature filled Successfully")
             count += 1
-    no_null = fill_feature(df, feature_ch , lis_fill)
+    no_null = fill_feature(df, feature_ch, lis_fill)
     text("")
     write(no_null)
 
@@ -221,7 +228,8 @@ def missing_values_filling_system(df , feature_tracker):
 #############################################################################################################################################################################################
 
 def features_overview_provider(df):
-    fig = suplots_maker_for_table_and_piechart(df , type_null = False , feature = None)
+    fig = suplots_maker_for_table_and_piechart(
+        df, type_null=False, feature=None)
     return fig
 
 #############################################################################################################################################################################################
@@ -232,7 +240,7 @@ def imbalanced_features_manager(df):
     if ls == []:
         info("There are no imbalanced Features in Dataset")
     else:
-        Markdown_Style("Imbalanced Features in Dataset are : " , 2)
+        Markdown_Style("Imbalanced Features in Dataset are : ", 2)
         dataframe(ls)
     text("")
     text("")
@@ -242,45 +250,42 @@ def imbalanced_features_manager(df):
 
 
 def useless_features_manager(df):
+    text("")
+    Markdown_Style("Useless Features :", type=2)
+    write("The features which have high unique values are:")
+    usl_df = useless_feat(df)
+
+    # appending the id column[if any] present in given df
+    # Checking for id column
+    lis_wow = []
+    for feature_name in df.columns:
+        if 'id' in feature_name.lower():
+            lis_wow.append(feature_name)
+    lis_wow.extend(list(usl_df['Feature']))
+    usl_df = pd.DataFrame(lis_wow, columns=['Feature'])
+
+    if len(usl_df) != 0:
         text("")
-        Markdown_Style("Useless Features :" , type = 2)
-        write("The features which have high unique values are:")
-        usl_df = useless_feat(df)
+        lis = []
+        for feature in usl_df["Feature"]:
+            if checkbox('Select to drop ' + feature):
+                drop_useless_feat(df, feature)
+                lis.append(feature)
+                success("Feature Dropped Successfully")
 
-
-        # appending the id column[if any] present in given df
-        # Checking for id column
-        lis_wow = []
-        for feature_name in df.columns:
-            if 'id' in feature_name.lower():
-                lis_wow.append(feature_name)
-        lis_wow.extend(list( usl_df['Feature'] ))
-        usl_df = pd.DataFrame( lis_wow , columns = ['Feature'] )
-
+        new = list(usl_df['Feature'])
+        for val in lis:
+            new.remove(val)
+        usl_df = usl_df[usl_df['Feature'].isin(
+            new)].reset_index().drop('index', axis=1)
 
         if len(usl_df) != 0:
-            text("")
-            lis = []
-            for feature in usl_df["Feature"]:
-                if checkbox('Select to drop ' +  feature):
-                    drop_useless_feat(df, feature)
-                    lis.append(feature)
-                    success("Feature Dropped Successfully")
-
-
-            new = list(usl_df['Feature'])
-            for val in lis:
-               new.remove(val)
-            usl_df = usl_df[ usl_df['Feature'].isin(new) ].reset_index().drop('index' , axis = 1)
-            
-
-            if len(usl_df) != 0:
-                write("Useless Features present in DataFrame : ")
-                write(usl_df)
-            text("")
-            text("")
-        else:
-            info("There are no useless Features in dataset")
+            write("Useless Features present in DataFrame : ")
+            write(usl_df)
+        text("")
+        text("")
+    else:
+        info("There are no useless Features in dataset")
 
 
 #############################################################################################################################################################################################
@@ -288,17 +293,19 @@ def useless_features_manager(df):
 
 def final_summary_provider(df):
 
-    markdown("____________________________________________________________________________")
+    markdown(
+        "____________________________________________________________________________")
     header("After Doing the Feature Engineering The dataset is now as below")
-    markdown("____________________________________________________________________________")
+    markdown(
+        "____________________________________________________________________________")
 
     text("")
     dataframe(df.head())
 
     if sum(df.isnull().sum()) == 0:
         text("")
-        success("Congrats Data Preprocessing phase is Done 🎉🎉. Now You can move on to next part, i.e , Doing EDA")
-
+        success(
+            "Congrats Data Preprocessing phase is Done 🎉🎉. Now You can move on to next part, i.e , Doing EDA")
 
     # text("")
     # success("Congrats Feature Engineering is Done 🎉🎉. Now You can move to next part, i.e , Doing EDA")
@@ -309,5 +316,4 @@ def final_summary_provider(df):
     b64 = base64.b64encode(csv.encode()).decode()
     href = f'<a href="data:file/csv;base64,{b64}">Download CSV File</a> (right-click and save as &lt;some_name&gt;.csv)'
     markdown(href, unsafe_allow_html=True)
-    df.to_csv("update.csv",index=False)
-
+    df.to_csv("update.csv", index=False)
